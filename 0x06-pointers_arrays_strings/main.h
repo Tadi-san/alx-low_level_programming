34ac2355c5a7f536a4037173f2e97ac6a09bdc89
Tadi-san <99659814+Tadi-san@users.noreply.github.com>
2022-03-24 13:48:48 -0900

    main

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,13 +1,4 @@
-#ifndef HOLBERTON_FUNTIONS
-#define HOLBERTON_FUNTIONS
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c);  /* An example function declaration */
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
