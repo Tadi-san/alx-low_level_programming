d79720f15efdee666e53c5037b7d4fcdd2fe3153
Tadi-san <tadiwosanegagregn@gmail.com>
2022-04-05 14:53:07 -0700

    kech

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,21 +1,16 @@
-#include <main.h>
+#include "main.h"
+#include <stdio.h>
 /**
- * main - Entry point with arguments
- *
- * @argc: counter of arguments passed
- * @argv: vector of strings with arguments passed
- *
+ * main - print name
+ * @argc: argument count
+ * @argv: argument vector for values
  * Return: Always 0 (Success)
- *
  */
+
 int main(int argc, char *argv[])
 {
-        (void)argc;
-
-        printf("%s\n", argv[0]);
-        return (0);
+	(void) argc;
+	printf("%s\n", *argv);
+	return (0);
 }
-~
-~
-~
 
